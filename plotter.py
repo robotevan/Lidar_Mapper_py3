3fb9fa2c8854060c762b2ec7316be10acfbed1e4
robotevan <robotevan24@gmail.com>
2020-04-30 02:38:27 -0400

    Matplotlib implementation, display point cloud

--- plotter.py
+++ plotter.py
@@ -1,31 +1,43 @@
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import math
+from matplotlib import cm
 from mpl_toolkits.mplot3d import axes3d, Axes3D
+import json
+import numpy as np
+
 
 class Plot:
+    """
+    Clas for plotting using matplotlib, however, open3d is desirable as gpu rendering is much faster
+    """
     def __init__(self):
         self.x_data = [] # create all x points
         self.y_data = [] # Create all y points
         self.z_data = [] # Create all z points
-        fig = plt.figure()
-        self.ax = Axes3D(fig)
-        plt.axis([-40, 40, -40, 40])
-        anim = animation.FuncAnimation(fig, self.animate_frame, interval=100)
+        self.fig = plt.figure()
+        self.ax = Axes3D(self.fig)
+        self.ax.set_xlabel("X AXIS")
+        self.ax.set_ylabel("Y AXIS")
+        self.ax.set_zlabel("Z AXIS")
+        self.open_data() # Grab data from json
+        # Convert to numpy array
+        self.x_data = np.array(self.x_data)
+        self.y_data = np.array(self.y_data)
+        self.z_data = np.array(self.z_data)
+        # Plot all data
+        self.ax.scatter(self.x_data, self.y_data, self.z_data, s=1)
+        # Show the final cloud
         plt.show()
 
-    def animate_frame(self, r, theta, phi):
-        x, y, z = self._sphere_to_cart_(r, theta, phi)
-        self.x_data.append(x)
-        self.y_data.append(y)
-        self.z_data.append(z)
-        self.ax.clear()
-        self.ax.plot(x, y, z)
-
-    def _sphere_to_cart_(self, r, theta, phi):
-        x = r*math.sin(theta)*math.cos(phi)  # get x
-        y = r*math.sin(theta)*math.sin(phi)  # get y
-        z = r*math.cos(theta)  # get z
-        return [x, y, z]  # return coord for point in cart
-
+    def open_data(self):
+        """
+        Retrieve data from json file
+        """
+        with open('coords.json', mode='r') as coords_json:
+            data = json.load(coords_json)
+            for point in data.keys():
+                if int(point)%2==0:
+                    coord = data.get(point)
+                    self.x_data.append(coord[0])
+                    self.y_data.append(coord[1])
+                    self.z_data.append(coord[2])
 
